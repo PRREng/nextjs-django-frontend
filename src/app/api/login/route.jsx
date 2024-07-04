edff18dc0560bb6851ca9b8036d92ce734f87ead
Pedro Rocha <pedro.atroneng@gmail.com>
2024-07-04 14:12:25 -0300

    Starting project

---
+++ src/app/api/login/route.jsx
@@ -0,0 +1,38 @@
+"use server";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+
+const DJANGO_API_LOGIN_URL = "http://127.0.0.1:8001/api/token/pair"
+
+export async function POST(request) {
+
+    const myAuthToken = cookies().get('auth-token');
+    console.log(myAuthToken);
+
+    const requestData = await request.json();
+    const jsonData = JSON.stringify(requestData);
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonData,
+    }
+    const response = await fetch(DJANGO_API_LOGIN_URL, requestOptions);
+    const responseData = await response.json();
+    if (response.ok) {
+        console.log("logged in")
+        const authToken = responseData.access
+        cookies().set({
+            name: 'auth-token',
+            value: authToken,
+            httpOnly: true, // limit client-side js
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV !== 'development',
+            maxAge: 3600,
+        })
+    }
+
+    return NextResponse.json({"hello": "world"}, {status: 200})
+}
