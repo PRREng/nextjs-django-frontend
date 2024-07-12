d4468367c128b65a3c541ae34ac2e103e3dcf3ad
Pedro Rocha Ribeiro <pr.rocha70@gmail.com>
2024-07-12 12:32:48 -0300

    just finished video at 4:03

--- src/app/api/clientes/route.jsx
+++ src/app/api/clientes/route.jsx
@@ -1,61 +1,17 @@
 import { getToken } from "@/lib/auth";
 import { NextResponse } from "next/server";
+import ApiProxy from "../proxy";
 
 const DJANGO_API_CLIENTES_URL = "http://127.0.0.1:8001/api/clientes/"
 
 export async function GET(request) {
-    const authToken = getToken();
-    if (!authToken) {
-        return NextResponse.json({}, {status: 401});
-    }
-
-    const options = { 
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${authToken}`
-        }
-    }
-    const response = await fetch(DJANGO_API_CLIENTES_URL, options);
-    console.log(response);
-    const result = await response.json();
-    let status = response.status;
-
-    return NextResponse.json({...result}, {status: status});
+    const {data, status} = await ApiProxy.get(DJANGO_API_CLIENTES_URL, true);
+    return NextResponse.json(data, {status: status});
 }
 
 export async function POST(request) {
-
     const requestData = await request.json();
-    const jsonData = JSON.stringify(requestData);
-
-    let headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-    };
-    const authToken = getToken();
-    if (authToken) {
-        headers["Authorization"] = `Bearer ${authToken}`;
-    }
-    console.log(`Auth Token: ${authToken}`);
-
-    const requestOptions = {
-        method: "POST",
-        headers,
-        body: jsonData,
-    }
-    const response = await fetch(DJANGO_API_CLIENTES_URL, requestOptions);
-    console.log(response.status);
-    try {
-        const responseData = await response.json();
-        console.log(responseData);
-    } catch (error) {
-        return NextResponse.json({}, {status: response.status});
-    }
-    if (response.ok) {
-        return NextResponse.json({}, {status: 200})
-    }
-
-    return NextResponse.json({}, {status: 400})
+    const {data, status} = await ApiProxy.post(DJANGO_API_CLIENTES_URL, 
+        requestData, true);
+    return NextResponse.json(data, {status: status});
 }
